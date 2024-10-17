05cf4f59c904b59ab57f457c7751acd7939676ae
Anvisimi <8444.sa@gmail.com>
2024-10-17 23:57:48 +0800

    added ELK

--- src/main/java/sg/edu/nus/iss/shopsmart_backend/HomeController.java
+++ src/main/java/sg/edu/nus/iss/shopsmart_backend/HomeController.java
@@ -1,11 +1,24 @@
 package sg.edu.nus.iss.shopsmart_backend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
+
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping("/")
     public String home() {
-        return "Welcome to Shopsmart!";
+        logger.info("{\"message\": \"Welcome to Central Hub"+"\"}");
+        return "Welcome to Central Hub!";
+    }
+
+
+    @RequestMapping("/home")
+    public String homeMethod() {
+        logger.info("{\"message\": \"Welcome to Central Hub Home"+"\"}");
+        return "Welcome to Central Hub Home!";
     }
 }
