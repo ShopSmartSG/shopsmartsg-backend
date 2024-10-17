e5b2a5e4e8114be854c85829ed2fb9dc303a4718
Anvisimi <8444.sa@gmail.com>
2024-10-17 23:57:48 +0800

    added ELK

--- src/test/java/sg/edu/nus/iss/shopsmart_backend/HomeControllerTest.java
+++ src/test/java/sg/edu/nus/iss/shopsmart_backend/HomeControllerTest.java
@@ -28,5 +28,21 @@ class HomeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("Welcome to Central Hub!"));
     }
+
+
+    @Test
+    void testHomeHub() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Welcome to Central Hub Home!"));
+    }
+
+    @Test
+    void testHomeHubWithEmptyPath() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Welcome to Central Hub Home!"));
+                .andExpect(content().string("Welcome to Central Hub!"));
+    }
 }
 
