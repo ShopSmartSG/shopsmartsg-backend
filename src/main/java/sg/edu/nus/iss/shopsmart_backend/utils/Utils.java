1846dd7022d1619e3b31ab2eabc8f81f8329e686
sasmitmati <sasmitmati98@gmail.com>
2024-10-26 00:41:33 +0800

    added service annotation in Utils

--- src/main/java/sg/edu/nus/iss/shopsmart_backend/utils/Utils.java
+++ src/main/java/sg/edu/nus/iss/shopsmart_backend/utils/Utils.java
@@ -9,8 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
 import sg.edu.nus.iss.shopsmart_backend.model.ApiRequestResolver;
 
+@Service
 public class Utils extends Constants{
     private static final Logger log = LoggerFactory.getLogger(Utils.class);
     public static HttpMethod getHttpMethod(String method){
