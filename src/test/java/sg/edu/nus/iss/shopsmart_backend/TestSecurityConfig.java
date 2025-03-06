aee4f88877b2d1a325663b7199a6c8c47408e9e4
sasmitmati <sasmitmati98@gmail.com>
2025-03-06 14:48:14 +0800

    support for home controller test using test security config.

---
+++ src/test/java/sg/edu/nus/iss/shopsmart_backend/TestSecurityConfig.java
@@ -0,0 +1,21 @@
+package sg.edu.nus.iss.shopsmart_backend;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@TestConfiguration
+public class TestSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
