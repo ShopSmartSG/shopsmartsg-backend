34a9261d4270688e177d7c2e2ccb86a7fe1a6588
sasmitmati <sasmitmati98@gmail.com>
2025-03-09 22:03:55 +0800

    added allow credentials as true for cors config

--- src/main/java/sg/edu/nus/iss/shopsmart_backend/config/SecurityConfig.java
+++ src/main/java/sg/edu/nus/iss/shopsmart_backend/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("https://app.shopsmartsg.com", "https://shopsmartsg.com", "https://accounts.google.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("DNT","User-Agent","X-Requested-With","If-Modified-Since","Cache-Control","Content-Type","Range","Authorization"));
         // Optionally set allow credentials, max age, etc.
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
